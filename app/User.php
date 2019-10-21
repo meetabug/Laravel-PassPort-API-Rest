c1182ec7f2e60bbc79e55fe2d645c908861eabaa
meetabug <1272526724@qq.com>
2019-10-21 23:45:13 +0800

    reset password

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Storage;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ Route::group([
         Route::get('user', 'AuthController@user');
     });
 });
+
+
+Route::group([
+    'namespace' => 'Auth',
+    'middleware' => 'api',
+    'prefix' => 'password'
+], function () {
+    Route::post('create', 'PasswordResetController@create');
+    Route::get('find/{token}', 'PasswordResetController@find');
+    Route::post('reset', 'PasswordResetController@reset');
+});
